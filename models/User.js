0c664239d03082f5a958193daf08016157ae4071
TyrWinDev <Tyrwinterhd@gmail.com>
2021-05-23 10:27:09 -0400

    Add new role to Schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
+  role: String,
   password: String,
 });
 

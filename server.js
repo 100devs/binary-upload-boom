36c98fe9c013e0de84f5b9b3a80c3ae4e46e4ace
Tundrian <antoniosansotta@gmail.com>
2022-09-10 10:41:41 -0400

    added local db config and env file

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.use("/post", postRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });

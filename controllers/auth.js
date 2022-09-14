6f3955b7b55a899590ae1645e45c97981cea61a7
Angra974 <thierryparlier97440@gmail.com>
2022-09-14 08:02:32 +0400

    Fix logout callback error

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,14 +44,19 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  })
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
+/**
+ * Log out.
+ */
+exports.logout = (req, res, next) => {
+  // Need a callback
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+     req.session.destroy((err) => {
+       if (err) console.log('Error : Failed to destroy the session during logout.', err);
+       req.user = null;
+     });
     res.redirect("/");
   });
 };

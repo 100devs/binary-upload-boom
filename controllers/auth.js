6f99691ff46f5eb184f032c77b54f98a1479b57c
Rowa <jrowah@gmail.com>
2022-09-14 21:56:19 +0300

    sets up .env

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,8 +44,12 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout();
+exports.logout = (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);

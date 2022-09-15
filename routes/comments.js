0e1ede74e0089eecd2674ef34e8fa25f9ddce9b9
mwmMecoy <macmecoy@gmail.com>
2022-09-14 19:23:14 -0500

    tCompleted comments integration

--- routes/comments.js
+++ routes/comments.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/multer");
-const postsController = require("../controllers/posts");
+const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Post Routes - simplified for now
-router.get("/:id", ensureAuth, postsController.getPost);
+//Comment Routes - simplified for now
 
-router.post("/createPost", upload.single("file"), postsController.createPost);
-
-router.put("/likePost/:id", postsController.likePost);
-
-router.delete("/deletePost/:id", postsController.deletePost);
+router.post("/createComment/:id", commentssController.createComment);
 
 module.exports = router;

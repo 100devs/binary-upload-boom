9b147930f06116cb689b1c0db8d61a86baa7f8d8
Kyle Clemente <kclemente11@me.com>
2022-09-15 18:26:47 -0500

    added comments feature

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  Comment: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", PostSchema);

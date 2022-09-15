f71418a5f0f36f917f4f65b69c192dffb0aaf09a
Jasmine <wrkerr9@gmail.com>
2022-09-15 16:56:57 -0700

    Just in case power goes out

---
+++ controllers/comments.js
@@ -0,0 +1,32 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.post_id,
+      });
+      console.log("Comment has been added!");
+      res.redirect(`/posts/${req.params.post_id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      await Post.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};

e60fb17c0b0f824dc2601d44a812d8c5cd962392
cb11cb12 <cb@CBs-MacBook-Pro.local>
2022-09-15 19:49:09 -0500

    working with comments

---
+++ controllers/comments.js
@@ -0,0 +1,41 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+     
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getComment: async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+//   deletePost: async (req, res) => {
+//     try {
+//       // Find post by id
+//       let post = await Post.findById({ _id: req.params.id });
+//       // Delete image from cloudinary
+//       await cloudinary.uploader.destroy(post.cloudinaryId);
+//       // Delete post from db
+//       await Post.remove({ _id: req.params.id });
+//       console.log("Deleted Post");
+//       res.redirect("/profile");
+//     } catch (err) {
+//       res.redirect("/profile");
+//     }
+//   },
+};

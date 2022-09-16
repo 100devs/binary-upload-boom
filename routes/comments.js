1a67f2e3c1c634b5defee28eb5a6924bd1506363
Olivia Bloway <blowayol@msu.edu>
2022-09-15 21:50:35 -0400

    added cloudinary for image upload

---
+++ routes/comments.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+//const postsController = require("../controllers/posts");
+const commentsController = ("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+
+//Post Routes - simplified for now
+//router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createComment/:id", commentsController.createComment);
+
+//router.put("/likePost/:id", postsController.likePost);
+
+//router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;

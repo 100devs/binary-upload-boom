3bbfd0f7b0d029a24f807907bdcf8010bf3ad613
Karlo Sebalj <karlo.sebalj@greyp.com>
2022-09-16 02:37:07 +0200

    Added comments part

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// Schema for comments
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  // link user to post so that we see the comments from user
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

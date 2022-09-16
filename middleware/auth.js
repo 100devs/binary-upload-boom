9cc7b946e07ad3ae123fdcd2be399feb3d49d1ae
Adam Robinson <robinson.db@gmail.com>
2022-09-16 13:46:42 -0400

    added comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,7 @@
 module.exports = {
   ensureAuth: function (req, res, next) {
+    console.log(req.user) // matches user id to session
+    console.log(req.session) // matches session to user id
     if (req.isAuthenticated()) {
       return next();
     } else {
--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 
-module.exports = multer({
+module.exports = multer({ // middleware for uploading images, holds a file in temporary storage
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);

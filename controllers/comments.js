63ae1312564d696bc9d5b1c75c08c9db55fc76c4
Jacob Asper <jacobasper191@gmail.com>
2022-09-15 22:50:47 -0400

    add comment to data base and add id to post

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,17 +1,27 @@
 const Comment = require("../models/Comment");
+//add post model
+const Post = require("../models/Post");
 
 module.exports = {
 	createComment: async (req, res) => {
 		try {
-			await Comment.create({
+			//create a new comment
+			const comment = await Comment.create({
 				title: req.body.title,
-				comment: req.body.comment,
-				post: req.params.id,
+				body: req.body.body,
 			});
+
+			//add the id of the comment to the array in the post document
+			Post.findById(req.params.id, (err, post) => {
+				post.comments.push(comment.id)
+				post.save()
+			})
+
 			console.log("Comment has been added!");
 			res.redirect("/profile");
 		} catch (err) {
 			console.log(err);
+			res.redirect("/profile");
 		}
 	},
 };

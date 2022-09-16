ebbe1238eae3ed43e3c70817c576393bc333a7ca
Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-09-16 12:14:07 -0400

    Fix getPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
+const User = require('../models/User');
 const Post = require("../models/Post");
+const Comment = require('../models/Comment');
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +23,11 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      console.log(post);
+      const user = await User.findById(post.user);
+      console.log(user);
+      const comments = await Comment.find({post: req.params.id});
+      res.render("post.ejs", { user: user, post: post, comments: comments});
     } catch (err) {
       console.log(err);
     }

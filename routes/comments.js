7a24da853acdf8902a21590e1898c00b7c28ee6f
devd00 <joydipkn@gmail.com>
2022-09-16 11:45:49 +0530

    added comments

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const commentController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+
+
+router.post("/createComment/:id", commentController.createComment);
+
+
+
+module.exports = router;

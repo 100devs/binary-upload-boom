6edd12b47617c01ff33efc2c6012cf4113073e6f
Aidan Williams <aidanw4@gmail.com>
2022-09-16 11:24:28 +0700

    add comment functionality on posts

---
+++ controllers/comments.js
@@ -0,0 +1,31 @@
+const Comment = require('../models/Comment');
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log('Comment has been added!');
+      res.redirect('/post/' + req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  //   deleteComment: async (req, res) => {
+  //     try {
+  //       // Find post by id
+  //       let post = await Post.findById({ _id: req.params.id });
+  //       // Delete image from cloudinary
+  //       await cloudinary.uploader.destroy(post.cloudinaryId);
+  //       // Delete post from db
+  //       await Post.remove({ _id: req.params.id });
+  //       console.log("Deleted Post");
+  //       res.redirect("/profile");
+  //     } catch (err) {
+  //       res.redirect("/profile");
+  //     }
+  //   },
+};

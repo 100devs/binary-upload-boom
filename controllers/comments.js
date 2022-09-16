6a5790c6d06a0ab7e543e005170872fd219ab03f
salgadodavid <salgado_d@outlook.com>
2022-09-15 22:38:35 -0400

    added comments

---
+++ controllers/comments.js
@@ -0,0 +1,43 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment, 
+        likes: 0,
+        post: req.params.id
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id); //check
+    } catch (err) {
+      console.log(err);
+    }
+  }
+//   likeComment: async (req, res) => {
+//     try {
+//       await Comment.findOneAndUpdate(
+//         { _id: req.params.id },
+//         {
+//           $inc: { likes: 1 },
+//         }
+//       ); 
+//       console.log("Comment Likes +1");
+//       res.redirect(`/post/${req.params.id}`);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   deleteComment: async (req, res) => {
+//     try {
+//       // Find post by id
+//       let comment = await Comment.findById({ _id: req.params.id });
+//       // Delete post from db
+//       await Comment.remove({ _id: req.params.id });
+//       console.log("Deleted Comment");
+//       res.redirect("/post/"); //check
+//     } catch (err) {
+//       res.redirect("/post"); //check
+//     }
+//   },
+};

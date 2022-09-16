ea09b6e6817dc080903d1a31eff1c8dd3968a365
Ahmad Yosry <87082240+Gonksy@users.noreply.github.com>
2022-09-16 04:44:43 +0200

    Added basic comment functionality

---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+// Comment Routes - simplified for now
+router.post("/:id", ensureAuth, commentsController.createComment);
+
+
+module.exports = router;

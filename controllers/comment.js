cf238761324c9999ac3ba973695e06d77534a218
Luis Sotelo <losjr4@gmail.com>
2022-09-15 18:10:07 -0700

    -m "add comment section to app"

---
+++ controllers/comment.js
@@ -0,0 +1,19 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        //grabs id from the post we passed in
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/" + req.params.id);
+      //take 'em back to that same post
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};

83d726c7ccfd5a52ae4e10d85c6138c2ed2e9b35
Joe Dukes (S1oop) <joedukes@gmail.com>
2022-09-15 20:30:00 -0400

    Comment controller first try

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,21 +1,18 @@
 const Comment = require("../models/Comment");
 
 module.exports = {
-  createPost: async (req, res) => {
+  createComment: async (req, res) => {
     try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
 
-      await Post.create({
-        title: req.body.title,
-        image: result.secure_url,
-        cloudinaryId: result.public_id,
-        caption: req.body.caption,
+      await Comment.create({
+        comment: req.body.comment,
         likes: 0,
+        dislikes: 0,
         user: req.user.id,
+        post: req.params.id,
       });
-      console.log("Post has been added!");
-      res.redirect("/profile");
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
     } catch (err) {
       console.log(err);
     }

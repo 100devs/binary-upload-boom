c391f8e85956d5d891ce1d7f92601760774429e3
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-09-18 01:50:51 +0300

    ability to add comments to a post

---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comments Routes - simplified for now
+
+router.post("/createComment/:id", ensureAuth, commentsController.createComment);
+
+module.exports = router;

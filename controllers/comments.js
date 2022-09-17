f1ff86b9fd82a8bd60273136ca3e53508590a1f8
valerievozza <valerievozza@gmail.com>
2022-09-17 13:38:55 -0700

    add comments

---
+++ controllers/comments.js
@@ -0,0 +1,20 @@
+const Post = require("../models/Post");
+const Comment = require('../models/Comment')
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      let post = await Post.findById(req.params.id).lean()
+
+      await Comment.create({
+        comment: req.body.text,
+        commenter: req.user.id,
+        post: req.params.id
+      });
+      console.log("Comment has been added!");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  // commenter: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "User",
+  // },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

2509fac2d75703fdf229e4c53dc67ebd61b1d5e1
jenniesf <jenniec2655@gmail.com>
2022-09-17 13:39:03 -0700

    add comment feature

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,15 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
+
+  // route: '/profile'
+  // desc: GET user profile
+  // render profile.ejs
   getProfile: async (req, res) => {
+    
+    //console.log(req.user)
     try {
       const posts = await Post.find({ user: req.user.id });
       res.render("profile.ejs", { posts: posts, user: req.user });
@@ -10,6 +17,10 @@ module.exports = {
       console.log(err);
     }
   },
+
+  // route: '/feed'
+  // desc: GET user feed
+  // render feed.ejs
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
@@ -18,19 +29,34 @@ module.exports = {
       console.log(err);
     }
   },
+
+
+  // route: '/post/:id'
+  // desc: GET one post and its comments
+  // render post.ejs
   getPost: async (req, res) => {
     try {
+
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comment = await Comment.find({ post: req.params.id });
+      
+      // console.log(comment)
+
+      res.render("post.ejs", { post: post, user: req.user , comment: comment });
     } catch (err) {
       console.log(err);
     }
   },
+
+  // route: '/post/createpost'
+  // desc: POST a post
+  // using form to create a post from profile.ejs
   createPost: async (req, res) => {
     try {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
+      // add to database
       await Post.create({
         title: req.body.title,
         image: result.secure_url,
@@ -45,6 +71,10 @@ module.exports = {
       console.log(err);
     }
   },
+
+  // route: '/post/likepost/:id'
+  // desc: PUT - like a post
+  // using form to like a post with method override
   likePost: async (req, res) => {
     try {
       await Post.findOneAndUpdate(
@@ -59,18 +89,70 @@ module.exports = {
       console.log(err);
     }
   },
+
+  // route: '/post/deletepost/:id'
+  // desc: DELETE - delete a post
+  // using form to delete a post with method override
   deletePost: async (req, res) => {
     try {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
+
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
+
       // Delete post from db
       await Post.remove({ _id: req.params.id });
+
       console.log("Deleted Post");
       res.redirect("/profile");
     } catch (err) {
       res.redirect("/profile");
     }
   },
+
+
+  // route: '/post/createcomment'
+  // desc: POST a comment to a post
+  // using form to create a comment on post.ejs
+  createComment: async (req, res) => {
+
+    // console.log( req.body )
+    try {
+      // add to database
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post:  req.params.id,
+        user: req.user.id,
+      });
+
+      console.log("Comment has been added!");
+
+      res.redirect(`/post/${req.params.id}`);
+
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+
+  // route: '/post/deletecomment'
+  // desc: POST a comment to a post
+  // using form to create a comment on post.ejs
+  deleteComment: async (req, res) => {
+    try {
+
+      // Delete comment from db
+      await Comment.remove({ _id: req.params.commentid });
+
+      console.log("Deleted Comment");
+
+      res.redirect(`/post/${req.params.postid}`);
+
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
+
 const upload = require("../middleware/multer");
+
 const postsController = require("../controllers/posts");
+
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-//Post Routes - simplified for now
+// Post Routes - simplified for now
+
+// SOCIAL MEDIA POSTS
+// get a post by id
 router.get("/:id", ensureAuth, postsController.getPost);
 
+// create a social media post
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
+// like a social media post - through form override 
 router.put("/likePost/:id", postsController.likePost);
 
+// delete a social media post - through form override
 router.delete("/deletePost/:id", postsController.deletePost);
+
+
+// COMMENTS
+// add a comment to a social media post
+router.post("/createComment/:id", postsController.createComment);
+
+// delete a comment to a social media post
+router.delete("/deleteComment/:postid/:commentid", postsController.deleteComment);
+
+
 
 module.exports = router;

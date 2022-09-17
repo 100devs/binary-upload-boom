0bed0dce7dfd2ccb570aa4555842707d036828d9
timdoan22 <timdoan01@gmail.com>
2022-09-17 19:38:33 -0400

    Comment schema in comment model

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// Comment schema 
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  // grab the post byreferencing the post schema
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

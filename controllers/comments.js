2e9fcb3663406f2655eea829941f32aebe5dc083
Austin_Curran <a.curran1192@gmail.com>
2022-09-18 11:16:25 -0600

    added ability to delete comments and fixed comments on other users posts

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,13 +6,27 @@ module.exports = {
     try {
         await Comment.create({
             comment: req.body.comment,
+            likes: 0,
             post: req.params.id,
-            createdAt: Date.now(),
+            user: req.user.id,
         });
-      console.log("Comment has been added!");
-      res.redirect("/post/"+req.params.id);
+    console.log("Comment has been added!");
+    res.redirect(`/post/${req.params.id}`);
     } catch (err) {
-      console.log(err);
+        console.log(err);
+    }
+},
+deleteComment: async (req, res) => {
+    try {
+        // Find post by id
+        let comment = await Comment.findById({ _id: req.params.id });
+        console.log({ comment })
+        // if req.user == comment.user
+        await Comment.deleteOne({ _id: req.params.id });
+        console.log("Deleted Comment");
+        res.redirect("/post/" + comment.post);
+    } catch (err) {
+        res.redirect("/profile");
     }
   },
 };

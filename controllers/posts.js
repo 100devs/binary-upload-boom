78a57ac3483f3bc2517b2e6ab39f179f7014783d
jennaly <jenna@wicot.io>
2022-09-18 12:10:16 -0700

    add comments to getPost controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,7 +21,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({createdAt: "asc"}).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }

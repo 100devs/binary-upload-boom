ace926a7a9052f3400a6d8dbf37b2c8bedbaeeed
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-09-19 01:17:41 +0300

    Added User variable

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,15 +1,16 @@
 const Comment = require("../models/Comment");
+const User = require("../models/User");
 
 module.exports = {
   createComment: async (req, res) => {
     try {
       // TODO — add new properties to each document
-      const commentUser = await User.findById({ user: req.user.id });
+      const commentUser = await User.findById(req.user.id);
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
-        createdBy: req.user.userName,
+        createdBy: commentUser.userName,
         createdById: req.user.id,
       });
       console.log("Comment has been added!");
@@ -21,6 +22,7 @@ module.exports = {
   deleteComments: async (req, res) => {
     try {
       await Comment.deleteOne({ _id: req.params.commentid });
+      console.log("comment removed");
       res.redirect("/post/" + req.params.postid);
     } catch (err) {
       console.log(err);

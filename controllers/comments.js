bd25ac59a7350a12c5d5aad56182997ea56b07c9
Mauro Colucci <colucci.mauro@gmail.com>
2022-09-18 00:50:59 -0300

    daily coding challenge - Arrays

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,12 +3,12 @@ const Comment = require("../models/Comment");
 module.exports = {
   createComment: async (req, res) => {
     try {
-      const comment = await Comment.create({
+      await Comment.create({
         comment: req.body.comment,
         post: req.params.id,
         createdBy: req.user.id
       });      
-      console.log("Comment has been added!", comment);
+      console.log("Comment has been added!");
       res.redirect(`/post/${req.params.id}`);
     } catch (err) {
       console.log(err);

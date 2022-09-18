205917dbb6071c3227da02d97cc6655ec265ecc9
timdoan22 <timdoan01@gmail.com>
2022-09-17 20:14:14 -0400

    Comments route

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+// Comment Routes - simplified for now
+
+// comment route to create post and requiring multer middleware to handle upload
+router.post("/createComment/:id", commentsController.createComment);
+
+
+module.exports = router;

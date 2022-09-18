b68ae2050ad94785551c729a11c951afc03b261b
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2022-09-18 21:55:59 +0100

    feature comments first pass

---
+++ controllers/comment.js
@@ -0,0 +1,35 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        user: req.user.id,
+        post: req.params.id
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    console.log(req.body)
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.body.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Comment likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};

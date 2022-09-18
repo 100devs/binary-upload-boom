6b7d311225fe7f2f84b91f7618f15c0c5d53ea0e
Daniel Hyres <danhyres@gmail.com>
2022-09-18 16:04:33 -0400

    add

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,10 +6,13 @@ module.exports = {
   createComment: async (req, res) => {
     try {
       //
+      // const commentUser = await User.findById(req.user.id)
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
+        createdBy: req.user.userName,
+        createdById: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect(`/post/${req.params.id}`);

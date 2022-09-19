17187caca845e9065c057efaf4364e2da4072c2f
ElektrazCode <ElektrazCode@gmail.com>
2022-09-19 19:36:07 -0400

    Added Delete/Update Comment Feature

--- controllers/comments.js
+++ controllers/comments.js
@@ -14,4 +14,30 @@ module.exports = {
       console.log(err);
     }
   },
+  likeComment: async (req, res) => {
+    try {
+      let comment = await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+      let comment = await Comment.findById({ _id: req.params.id });
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      res.redirect(`/post/${comment.post}`);
+    }
+  }
 };

32e7fab980fb884f582a8b1d93d9aa52a41f190a
Jamie Lou Brolund <94914187+JamieLouBrolund@users.noreply.github.com>
2022-09-19 11:03:33 -0500

    comments added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comments");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -12,7 +13,12 @@ module.exports = {
   },
   getFeed: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+      const posts = await Post.find()
+        .sort(
+          //find and sort in decending order
+          { createdAt: "desc" }
+        )
+        .lean(); // keep it lean, just give me the object, makes things go faster (mongoose docs)
       res.render("feed.ejs", { posts: posts });
     } catch (err) {
       console.log(err);
@@ -21,7 +27,17 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({ post: req.params.id })
+        .sort(
+          //find and sort in decending order
+          { createdAt: "desc" }
+        )
+        .lean(); // keep it lean, just give me the object, makes things go faster (mongoose docs)
+      res.render("post.ejs", {
+        post: post,
+        user: req.user,
+        comments: comments,
+      });
     } catch (err) {
       console.log(err);
     }

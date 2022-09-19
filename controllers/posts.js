38c6b7e56addc79d144bcb6b4c7d949355858638
Bianca Talabe <btalabe@gmail.com>
2022-09-19 18:38:57 -0400

    deleting comments added

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,7 +22,7 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "asc" }).lean();
       res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
--- routes/comments.js
+++ routes/comments.js
@@ -6,6 +6,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Comment Routes - simplified for now
 router.post("/createComment/:id",  commentsController.createComment);
 
+router.delete("/deleteComment/:postId/:commentId", commentsController.deleteComment)
 
 
 module.exports = router;

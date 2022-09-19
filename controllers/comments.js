24d609df73be045d5e6a08edccea81b85389fba8
Rachel Fairchild <rfair009@gmail.com>
2022-09-19 12:57:55 -0400

    Add additional comment details to comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,10 +3,13 @@ const Comment = require("../models/Comment");
 module.exports = {
   createComment: async (req, res) => {
     try {
+      // const commentUser = await User.findById(req.user.id)
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
+        createdBy: req.user.userName,
+        createdByID: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);

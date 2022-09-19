3bce2c461401a667a0722b740f49b96350241af3
ned-park <87483870+ned-park@users.noreply.github.com>
2022-09-19 13:23:24 -0400

    Add and delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -8,4 +8,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.post("/createComment/:id", commentsController.createComment);
 
+router.delete("/deleteComment/:id", commentsController.deleteComment)
+
 module.exports = router;

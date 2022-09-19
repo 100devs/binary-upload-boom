ac27596a5df552347bfa780787a70042835046f0
jennaly <106183040+jennaly@users.noreply.github.com>
2022-09-19 10:04:27 -0700

    add delete comment feature to controller, set up route to invoke deleteComment function, display form to delete a comment on the client in post view file

--- routes/comments.js
+++ routes/comments.js
@@ -5,4 +5,6 @@ const commentsController = require("../controllers/comments");
 //Comment Routes - simplified for now
 router.post("/createComment/:id", commentsController.createComment);
 
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComment);
+
 module.exports = router;

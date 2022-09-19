8b0721b70a81ee1053b35b044f647a1e4eb0d37b
Anthony Moran <anthony771998@gmail.com>
2022-09-19 17:28:43 -0400

    Added comment form

--- controllers/posts.js
+++ controllers/posts.js
@@ -23,7 +23,7 @@ module.exports = {
     try {
       const post = await Post.findById(req.params.id);
       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
-      res.render("post.ejs", { post: post, user: req.user, comment : comments });
+      res.render("post.ejs", { post: post, user: req.user, comment: comments });
     } catch (err) {
       console.log(err);
     }

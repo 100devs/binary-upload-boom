547be47a6d618c8c9c3896d4264aabefb1a1d8b7
Lucas Faustman <lucasfaustman@gmail.com>
2022-09-18 20:33:46 -0700

    Commiting to try and fix comment filtering issue inside of Ejs

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,7 +15,8 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      const comments = await Comment.find().sort({ createdAt: "desc" }).lean();
+      res.render("feed.ejs", { posts: posts, comments: comments });
     } catch (err) {
       console.log(err);
     }

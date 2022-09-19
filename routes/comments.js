2a6403fff4041a4fc4a282750d5279c7e3e8c564
Rachel Fairchild <rfair009@gmail.com>
2022-09-19 13:46:33 -0400

    Add comment delete support to controller and routes, update header partials

--- routes/comments.js
+++ routes/comments.js
@@ -4,9 +4,7 @@ const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Comment Routes - simplified for now
-
 router.post("/createComment/:id", commentsController.createComment);
-
-    // router.delete("/deletePost/:id", postsController.deletePost);
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComments);
 
 module.exports = router;

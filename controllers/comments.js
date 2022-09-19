add355c4b9fa1b90cfca4c987d96448d859753d4
Chase O'Hara <stanleycohara@gmail.com>
2022-09-19 11:50:43 -0400

    added comments/ delete and like comments

---
+++ controllers/comments.js
@@ -0,0 +1,50 @@
+const Post = require('../models/Post');
+const Comment = require('../models/Comment');
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log('Comment has been added!');
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  likeComment: async (req, res) => {
+    try {
+      let comment = await Comment.findById({ _id: req.params.id });
+      await Comment.findOneAndUpdate(
+        // find post to update by ID,
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 }, // and increment by 1
+        }
+      );
+      console.log('Likes +1');
+      res.redirect(`/post/${req.query.postid}`); // Redirect back to post page
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+
+      let comment = await Comment.findById({ _id: req.params.id });
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log('Deleted Comment');
+      console.log(req.params.id);
+      res.redirect(`/post/${req.query.postid}`);
+    } catch (err) {
+      res.redirect(err);
+    }
+  },
+};
---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const commentsController = require('../controllers/comments');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
+
+//Comments Routes - simplified for now
+
+router.post('/add/:id', commentsController.createComment);
+
+router.delete('/delete/:id', commentsController.deleteComment);
+
+router.put("/like/:id", commentsController.likeComment);
+
+module.exports = router;

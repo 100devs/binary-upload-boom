f3fbc151ea7626ed4b618c9bcf13e05ce4024c02
Ticia Francisco <ticiafrancisco@gmail.com>
2022-09-20 15:48:47 -0700

    Comment out unused user object

--- models/Comment.js
+++ models/Comment.js
@@ -13,10 +13,10 @@ const CommentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
-  user: {
+/*   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-  },
+  }, */
   createdAt: {
     type: Date,
     default: Date.now,

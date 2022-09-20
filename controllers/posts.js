26bdc7b364eb51dd8cfcbb81630ffefa07bc20ef
NickRoccodev11 <nchls.rcc@gmail.com>
2022-09-20 19:58:00 -0400

    added comments, delete comments, changed likes to array

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,8 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+
+
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +24,9 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({ post: req.params.id }).sort({ createdAt: "asc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
+     console.log(req.user.userName)
     } catch (err) {
       console.log(err);
     }
@@ -36,7 +41,6 @@ module.exports = {
         image: result.secure_url,
         cloudinaryId: result.public_id,
         caption: req.body.caption,
-        likes: 0,
         user: req.user.id,
       });
       console.log("Post has been added!");
@@ -48,9 +52,10 @@ module.exports = {
   likePost: async (req, res) => {
     try {
       await Post.findOneAndUpdate(
+
         { _id: req.params.id },
         {
-          $inc: { likes: 1 },
+          $push: {likes: req.params.username} ,
         }
       );
       console.log("Likes +1");
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
+const postRoutes = require("./routes/post");
+const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -56,6 +57,8 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
+
 
 //Server Running
 app.listen(process.env.PORT, () => {

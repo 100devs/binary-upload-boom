cbd9cbaabfd3e88167add09ab50bccb35b16ee03
Ana URL <ana.urlic@gmail.com>
2022-09-20 07:35:06 -0400

    Add comment feature

---
+++ routes/comment.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comment");
+
+//Post Routes - simplified for now
+// router.get("/:id", commentsController.getComment);
+
+router.post("/createComment/:id", commentsController.createComment);
+
+// router.put("/likeComment/:id", commentsController.likeComment);
+
+// router.delete("/deleteComment/:id", commentsController.deleteComment);
+
+module.exports = router;

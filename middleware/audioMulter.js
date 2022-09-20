2272af8ccfff7f0471020536c4a9bcacb6b7845b
eggsmayhem <flipmybit@protonmail.com>
2022-09-19 21:58:41 -0400

    cleaned up merge

--- middleware/audioMulter.js
+++ middleware/audioMulter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const audioMulter = require("multer");
 const path = require("path");
 
@@ -13,19 +12,3 @@ module.exports = audioMulter({
     cb(null, true);
   },
 });
-=======
-const audioMulter = require("multer");
-const path = require("path");
-
-module.exports = audioMulter({
-  storage: audioMulter.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (file.mimetype !== "audio/mp3" && file.mimetype !== "audio/mpeg" && file.mimetype !== "audio/wav" && file.mimetype !== "audio/mp4") {
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
-    cb(null, true);
-  },
-});
->>>>>>> ac686934591dda022baf4734610461a6d06a7d02

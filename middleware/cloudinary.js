20e19ee557ba47988d37a950de43eaf028834151
Tyler Perata <tylerperata@gmail.com>
2022-09-19 21:27:20 -0700

    Change .env location

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,7 +1,7 @@
 // Init cloudinary
 const cloudinary = require("cloudinary").v2;
 
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,

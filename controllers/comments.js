ac62729dc864d23efd790360073a2f74d1eca19a
timdoan22 <timdoan01@gmail.com>
2022-09-20 02:26:51 -0400

    delete button for comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -20,16 +20,26 @@ module.exports = {
     try {
       // Update the like counter by one
       await Comment.findOneAndUpdate(
-        { _id: req.params.id },
+        { _id: req.params.commentid },
         {
           $inc: { likes: 1 },
         }
       );
       console.log("Comment likes +1");
       // return the user to the same post page
-      res.redirect(`/post/${req.params.id}`);
+      res.redirect("/post/"+req.params.postid);
     } catch (err) {
       console.log(err);
     }
-  }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Delete comment from db
+      await Comment.deleteOne({ _id: req.params.commentid });
+      console.log("Deleted Comment");
+      res.redirect("/post/"+req.params.postid);
+    } catch (err) {
+      res.redirect("/post/"+req.params.postid);
+    }
+  },
 }
--- routes/comments.js
+++ routes/comments.js
@@ -8,7 +8,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 // comment route to create post and requiring multer middleware to handle upload
 router.post("/createComment/:id", commentsController.createComment);
 
-router.put("/likeComment/:id", commentsController.likeComment);
+router.put("/likeComment/:postid/:commentid", commentsController.likeComment);
 
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComment)
 
 module.exports = router;

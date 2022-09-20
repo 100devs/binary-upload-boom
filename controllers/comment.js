17b3c533595cb88d7c9e1d6c1f4c7e570d3f9749
Ana URL <ana.urlic@gmail.com>
2022-09-20 07:46:29 -0400

    Remove unnecessary code

--- controllers/comment.js
+++ controllers/comment.js
@@ -2,14 +2,7 @@ const Post = require("../models/Post");
 const Comment = require("../models/Comment")
 
 module.exports = {
-//   getComment: async (req, res) => {
-//     try {
-//       const comment = await Post.findById(req.params.id);
-//       res.render("post.ejs", { post: post, user: req.user });
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   },
+
   createComment: async (req, res) => {
     try {
       await Comment.create({
@@ -23,32 +16,5 @@ module.exports = {
       console.log(err);
     }
   },
-//   likePost: async (req, res) => {
-//     try {
-//       await Post.findOneAndUpdate(
-//         { _id: req.params.id },
-//         {
-//           $inc: { likes: 1 },
-//         }
-//       );
-//       console.log("Likes +1");
-//       res.redirect(`/post/${req.params.id}`);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   },
-//   deletePost: async (req, res) => {
-//     try {
-//       // Find post by id
-//       let post = await Post.findById({ _id: req.params.id });
-//       // Delete image from cloudinary
-//       await cloudinary.uploader.destroy(post.cloudinaryId);
-//       // Delete post from db
-//       await Post.remove({ _id: req.params.id });
-//       console.log("Deleted Post");
-//       res.redirect("/profile");
-//     } catch (err) {
-//       res.redirect("/profile");
-//     }
-//   },
+  
 };
--- routes/comment.js
+++ routes/comment.js
@@ -2,13 +2,6 @@ const express = require("express");
 const router = express.Router();
 const commentsController = require("../controllers/comment");
 
-//Post Routes - simplified for now
-// router.get("/:id", commentsController.getComment);
-
 router.post("/createComment/:id", commentsController.createComment);
-
-// router.put("/likeComment/:id", commentsController.likeComment);
-
-// router.delete("/deleteComment/:id", commentsController.deleteComment);
 
 module.exports = router;

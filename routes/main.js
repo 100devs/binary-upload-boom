3fd2a9d0e80b70caf25b1410a38bbd44bfc4cf38
Allister Sascha <101225242+allistersascha@users.noreply.github.com>
2022-09-20 17:59:29 -0500

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth");
-const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const express = require("express"); //use expresssss
+const router = express.Router(); //use it for your router
+const authController = require("../controllers/auth"); //make sure they're signed in
+const homeController = require("../controllers/home"); //use the home controller
+const postsController = require("../controllers/posts"); //use the post controller
+
+const { ensureAuth, ensureGuest } = require("../middleware/auth"); //use ya midware to auth
 
 //Main Routes - simplified for now
-router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
-router.get("/feed", ensureAuth, postsController.getFeed);
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/signup", authController.getSignup);
+router.get("/", homeController.getIndex); //root route root route
+router.get("/profile", ensureAuth, postsController.getProfile); //once you're logged in proper you get ya prof
+router.get("/feed", ensureAuth, postsController.getFeed); //....aaand your feed
+router.get("/login", authController.getLogin); //but you have to login
+router.post("/login", authController.postLogin); 
+router.get("/logout", authController.logout); //and out
+router.get("/signup", authController.getSignup); //and sign up to begin with 
 router.post("/signup", authController.postSignup);
+
 
 module.exports = router;

5754d531dc70b4ed58312d2d0d936328835e8649
Leah Godfrey <missbabelfish@gmail.com>
2022-09-20 11:12:06 -0700

    add env variables. working build

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -4,8 +4,8 @@ require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
 });
 
 module.exports = cloudinary;

d8ab56cd8cbe5b2cc0cac6998f4457d4fe834185
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-09-21 22:46:04 +0300

    added comments feature

---
+++ routes/comment.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comment");
+
+router.post("/createComment/:id", commentsController.createComment);
+
+module.exports = router;

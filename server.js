3e29ece579961930620316765442d77790ed4c93
Julien Mellon <mellon.julien@gmail.com>
2022-09-21 11:59:36 -0400

    added PORT number to console.log

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.use("/comment", commentRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-	console.log("Server is running, you better catch it!");
+	console.log(`Server is running on PORT ${process.env.PORT}, you better catch it!`);
 });

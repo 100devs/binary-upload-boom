e5cdd95d0783b5992167688d9695ce0babbcbcfb
cesar-codes <cesar.avina@gmail.com>
2022-09-21 12:43:38 -0400

    add comments like button

--- controllers/comments.js
+++ controllers/comments.js
@@ -14,4 +14,18 @@ module.exports = {
       console.log(err);
     }
   },
-};
+},
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+}

a8c09127a1b457504848b45ec98376786da40f64
Nima <nima97e@gmail.com>
2022-09-21 16:54:03 +0430

    add deleting ability to comments and to see who made the comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const commentsController = require("../controllers/comments");
 
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
@@ -8,6 +7,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //comment Routes - simplified for now
 
 router.post("/createComment/:id", commentsController.createComment);
+router.delete("/deleteComment/:postId/:commentId", commentsController.deleteComment);
 
 
 module.exports = router;

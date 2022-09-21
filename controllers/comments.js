d84a64704cb6ccc0fc2f8f99a4d3c1bd3a631f7a
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-09-20 23:51:40 -0400

    added comments controller

---
+++ controllers/comments.js
@@ -0,0 +1,59 @@
+// const express = require("express");
+// const router = express.Router();
+// const commentsController = require("../controllers/comments");
+// const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+
+
+// router.get("/:id", ensureAuth, commentsController.getPost);
+
+// router.post("/createPost", upload.single("file"), commentsController.createPost);
+
+// router.put("/likePost/:id", commentsController.likePost);
+
+// router.delete("/deletePost/:id", commentsController.deletePost);
+
+
+const Comment = require("../models/Comment");
+
+module.exports = {
+ createComment: async (req, res) => {
+    try {
+         await Comment.create({ 
+             comment: req.body.comment,
+             likes: 0,
+             user: req.user.id
+         });
+        console.log("Comment has been added!");
+        res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+        await Comment.findOneAndUpdate(
+            { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/Comment/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find Comment by id
+      let Comment = await Comment.findById({ _id: req.params.id });
+      // Delete Comment from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};

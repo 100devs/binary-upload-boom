b885fea3b1ddf32e3f089b221334042d16645964
Julien Mellon <mellon.julien@gmail.com>
2022-09-21 13:25:53 -0400

    implemented soft delete, but what do I do with em?

--- controllers/comment.js
+++ controllers/comment.js
@@ -29,23 +29,20 @@ module.exports = {
 			console.log(err);
 		}
 	},
-	softDeleteComment: ()=>{
-		likePost: async (req, res) => {
-			try {
-				await Comment.findOneAndUpdate(
-					{ _id: req.params.id },
-					{
-						$inc: { likes: 1 },
-					}
-					);
-					console.log("Likes +1");
-					console.log('Comment ${commentID} has been deleted')
-					res.redirect(`/post/${req.params.id}`);
-			} catch (err) {
-				console.log(err);
-			}
+	softDeleteComment: async (req, res)=>{
+		try {
+			await Comment.findOneAndUpdate(
+				{ _id: req.params.idComment },
+				{
+					deleted: true
+				}
+			);
+			console.log("Comment Deleted. Well... at least as far as a user is concerned");
+			console.log(`post ${req.body.postid}`)
+			res.redirect(`/post/${req.body.postid}`);
+		} catch (err) {
+			console.log(err);
 		}
-	
 	}
 		
 };

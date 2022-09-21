d1e54d7ef886ff1282402338c50402febdf2a55a
Angra974 <thierryparlier97440@gmail.com>
2022-09-21 08:53:02 +0400

    Modal modified

--- public/js/modal.js
+++ public/js/modal.js
@@ -5,27 +5,52 @@ exampleModal.addEventListener("show.bs.modal", function(event) {
   const form = document.querySelector("#commentForm")
   const commentId = button.getAttribute("data-bs-id");
 // Extract info from data-bs-* attributes
-  const modalTitle = exampleModal.querySelector("#modalTitle");
-console.log(commentId)
-  const el = document.createElement("input");
-  el.type = 'hidden'
-  el.name = "id";
-  el.value = commentId
-  form.appendChild(el)
+  const modalBody = exampleModal.querySelector("#modalBody");
+
+  // check if hidden comment id is present 
+  const hiddenId = exampleModal.querySelector('input[name="id"]')
+  if(hiddenId !== null) {
+	  // change it value if found
+	  hiddenId.value = commentId
+  } else {	  
+  // add the current comment id to our form
+	  const el = document.createElement("input");
+	  el.type = 'hidden'
+	  el.name = "id";
+	  el.value = commentId
+	  form.appendChild(el)
+  }
+
   form.method = 'post';
 
 
-  if(button.classList.contains('text-danger')) { // for delete
-//  modalTitle.innerHTML = '';
-  modalTitle.textContent = "Delete this comment?";
-  // delete a group after confirmation
-  form.action = "/comment/deleteComment"
-  } else {
-      // For comment modification
+  if(button.classList.contains('editComment')) { // for delete
+	addModalTextArea(modalBody)
+		// For comment modification
   const commentText = exampleModal.querySelector("#commentArea");
   commentText.value = button.parentElement.previousElementSibling.textContent;
   form.action = "/comment/editComment"
-}
+
+  } else if(button.classList.contains('addComment')) { // for creating a new comment
+	// clear the body
+	addModalTextArea(modalBody)
+	form.action = '/comment/createComment'
+  
+  } else {
+		modalBody.textContent = "Delete this comment?";
+		  // delete a group after confirmation
+  form.action = "/comment/deleteComment"
+
+	}
 
 });
 
+
+function addModalTextArea(modalBody) {
+	
+	modalBody.innerHTML = '';
+	// add proper textara
+	modalBody.insertAdjacentHTML('afterbegin',
+		'<label class="form-label" for="comment">Your comment</label><textarea class="form-control col-xs-12" rows="7" cols="50" name="comment" id="commentArea"></textarea>');
+
+}

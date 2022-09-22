6e3a302c27768c3a9e1baa4250ed86b9f039eec8
[Chris Becker} <oneringtobeckerthem@gmail.com>
2022-09-22 10:29:27 -0500

    add comments with username and delete to posts

---
+++ models/Comments.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  }, 
+  user: {
+    type: String,
+    ref: "User",
+  }, 
+  userByID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);

dc27979fdd8dd38889e2db18241cdbf0f7e8c7b1
hammock-sleeper <hammock_sleeper@tutanota.com>
2022-09-22 17:40:40 -0400

    add comments - I completed the challenge: 5 - I feel good about my code: 5

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

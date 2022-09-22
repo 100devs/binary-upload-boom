77f1e4453a7d08253b2011229db5b26727fc74df
Adam Shea <avidworks@live.com>
2022-09-21 21:29:12 -0400

    resolve auth.js conflict

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,15 +45,13 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-	req.logout((err) => {
-		if (err) {
-			console.log(err);
-		}
-		req.session.destroy((err) => {
-			if (err) console.log("Error : Failed to destroy the session during logout.", err);
-			req.user = null;
-			res.redirect("/");
-		});
+	req.logout(() => {
+		console.log("User has logged out.");
+	});
+	req.session.destroy((err) => {
+		if (err) console.log("Error : Failed to destroy the session during logout.", err);
+		req.user = null;
+		res.redirect("/");
 	});
 };
 

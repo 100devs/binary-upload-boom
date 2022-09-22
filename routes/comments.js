4be17c1046128c7df94df989c740952d06a811cf
a-cole55 <arniqua_c@yahoo.com>
2022-09-21 19:09:04 -0500

    added comment functionality/feature

---
+++ routes/comments.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+router.post("/createComment/:id", commentsController.createComment);
+
+module.exports = router;

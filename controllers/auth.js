8fb401623b147e053a3c402ad844c684f4b18176
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-09-22 14:49:14 -0500

    got logout to work and removed todos ejs file

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,6 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  req.logout();
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);

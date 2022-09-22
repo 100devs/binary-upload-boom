414132124bcd3720a4041b1885fbeb5b4c788f37
John Malapit <johncmalapit@gmail.com>
2022-09-21 21:56:40 -0400

    add csp tag to html

--- controllers/posts.js
+++ controllers/posts.js
@@ -58,8 +58,6 @@ module.exports = {
         { $match: { user: req.user._id } },
         { $group: { _id: '$user', total: { $sum: '$likes' } } },
       ]);
-      console.log('total number of likes: ', likes[0] ? likes[0].total : 0);
-      console.log(posts);
       res.render('feed.ejs', {
         posts: posts,
         likes: likes[0] ? likes[0].total : 0,

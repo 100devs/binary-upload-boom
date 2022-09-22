ff0e81059841f4ae48ac76bf88521e7aa25d9bf8
Ashleigh Halverstadt <ashleigh.halverstadt@gmail.com>
2022-09-22 15:08:30 -0600

    added comment functionality, including ability to like and delete comments

---
+++ controllers/comments.js
@@ -0,0 +1,41 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        user: req.user.id
+      });
+      console.log("Comment has been added!");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Comment likes +1");
+      res.redirect('back');
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted comment");
+      res.redirect('back');
+    } catch (err) {
+      res.redirect('back');
+    }
+  },
+};

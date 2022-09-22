0eb95019ff7e0b507cb5f3879ced71b858f55556
MK Frechette <mollykatefrechette@gmail.com>
2022-09-22 19:13:09 -0400

    adding comments

---
+++ routes/comments.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comment Routes - simplified for now
+/*router.get("/:id", ensureAuth, commentsController.getComment);*/
+
+router.post("/createComment/:id", commentsController.createComment);
+
+/*router.put("/likeComment/:id", commentsController.likeComment);
+
+router.delete("/deleteComment/:id", commentsController.deleteComment);*/
+
+module.exports = router;

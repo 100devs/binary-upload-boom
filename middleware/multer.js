86b18238bddf041cd3d6c085e9003ab36cda141e
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-09-22 17:38:18 -0500

    Add heic support to multer middleware

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,8 +4,8 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+    let ext = path.extname(file.originalname).toLowerCase();
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== '.heic') {
       cb(new Error("File type is not supported"), false);
       return;
     }

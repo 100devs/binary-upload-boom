06b77723fc662a8d89b49d513c4e84403caf5b64
sashavining <96848086+sashavining@users.noreply.github.com>
2022-09-22 19:35:49 -0400

    finished submission

---
+++ controllers/comments.js
@@ -0,0 +1,17 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};

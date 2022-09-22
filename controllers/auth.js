caf46bdcd76bb3ab18bc5d63528ee2c017bff46d
Omar Hernadez <omarh619@yahoo.com>
2022-09-22 15:26:12 -0700

    Comments added

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,7 +44,7 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
+/* exports.logout = (req, res) => {
   req.logout();
   req.session.destroy((err) => {
     if (err)
@@ -52,7 +52,17 @@ exports.logout = (req, res) => {
     req.user = null;
     res.redirect("/");
   });
-};
+}; */
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log('User has logged out.')
+  })
+  req.session.destroy((err) => {
+    if (err) console.log('Error : Failed to destroy the session during logout.', err)
+    req.user = null
+    res.redirect('/')
+  })
+}
 
 exports.getSignup = (req, res) => {
   if (req.user) {
---
+++ controllers/comments.js
@@ -0,0 +1,66 @@
+;
+const Comment = require("../models/Comment");
+module.exports = {
+
+  createComment: async (req, res) => {
+    try {
+     await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/" + req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  likeComment: async (req, res) => {
+    try {
+      let comment = await Comment.findById(req.params.id)
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  dislikeComment: async (req, res) => {
+    try {
+      let comment = await Comment.findById(req.params.id)
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: -1 },
+        }
+      );
+      console.log("dislike -1");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+      let comment = await Comment.findById({ _id: req.params.id });
+    
+      // Delete post from db
+      await Comment.findOneAndDelete({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect(`/post/${comment.post}`);
+    } catch (err) {
+      res.redirect(`/post/${comment.post}`);
+    }
+  },
+};
+
+

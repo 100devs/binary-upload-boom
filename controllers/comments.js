d69268631a390d7508e5ccb8b36aadcb57b8d5f1
NickSramcik <Nick.Sramcik@gmail.com>
2022-09-22 14:28:57 -0700

    Adds comments and usernames

---
+++ controllers/comments.js
@@ -0,0 +1,72 @@
+// const cloudinary = require("../middleware/cloudinary");
+const Comment = require("../models/Comment");
+
+module.exports = {
+  // getProfile: async (req, res) => {
+  //   try {
+  //     const posts = await Post.find({ user: req.user.id });
+  //     res.render("profile.ejs", { posts: posts, user: req.user });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  // getFeed: async (req, res) => {
+  //   try {
+  //     const comments = await Comment.find({ user: req.user.id }).sort({ createdAt: "desc" }).lean();
+  //     res.render("feed.ejs", { posts: posts });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  // getComment: async (req, res) => {
+  //   try {
+  //     const post = await Comment.findById(req.params.id);
+  //     res.render("post.ejs", { post: post, user: req.user });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        parentPost: req.params.id,
+        likes: 0,
+        user: req.user.id,
+        userName: req.user.userName,
+      });
+      console.log("Comment has been added!");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  // likeComment: async (req, res) => {
+  //   try {
+  //     await Post.findOneAndUpdate(
+  //       { _id: req.params.id },
+  //       {
+  //         $inc: { likes: 1 },
+  //       }
+  //     );
+  //     console.log("Likes +1");
+  //     res.redirect(`/post/${req.params.id}`);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  // deletePost: async (req, res) => {
+  //   try {
+  //     // Find post by id
+  //     let post = await Post.findById({ _id: req.params.id });
+  //     // Delete image from cloudinary
+  //     await cloudinary.uploader.destroy(post.cloudinaryId);
+  //     // Delete post from db
+  //     await Post.remove({ _id: req.params.id });
+  //     console.log("Deleted Post");
+  //     res.redirect("/profile");
+  //   } catch (err) {
+  //     res.redirect("/profile");
+  //   }
+  // },
+};
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({ parentPost: req.params.id }).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }
@@ -38,6 +40,7 @@ module.exports = {
         caption: req.body.caption,
         likes: 0,
         user: req.user.id,
+        userName: req.user.userName,
       });
       console.log("Post has been added!");
       res.redirect("/profile");

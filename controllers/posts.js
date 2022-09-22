393a6dcb8d81e93f1f9bc1d262d440ab6c01b352
ahmedDevs <ahmed.soft.dev@gmail.com>
2022-09-22 13:52:37 -0400

    Add comments and delete functionalities.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -21,7 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }
@@ -30,7 +32,6 @@ module.exports = {
     try {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
-
       await Post.create({
         title: req.body.title,
         image: result.secure_url,
@@ -39,6 +40,7 @@ module.exports = {
         likes: 0,
         user: req.user.id,
       });
+      
       console.log("Post has been added!");
       res.redirect("/profile");
     } catch (err) {

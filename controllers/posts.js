673ee62636597a3fe77db45a63a19ad415d0ce3c
atlasmac <mckinleyatlas@gmail.com>
2022-09-22 16:25:16 -0600

    Add comments with user names.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,12 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
       const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { posts: posts, user: req.user});
     } catch (err) {
       console.log(err);
     }
@@ -21,7 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).populate('user').lean();
+      res.render("post.ejs", { post: post, comments : comments, user: req.user });
     } catch (err) {
       console.log(err);
     }
---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//comments Routes - simplified for now
+router.post("/createComment/:id", commentsController.createComment);
+
+// router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;

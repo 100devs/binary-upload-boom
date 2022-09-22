947e5704b1a37ad09e00666f00aaf18ef4f41862
gitcyco <gitcyco@users.noreply.github.com>
2022-09-22 00:41:12 -0700

    setup navbar

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 module.exports = {
   getIndex: (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { user: req.user });
   },
 };
--- controllers/posts.js
+++ controllers/posts.js
@@ -14,7 +14,7 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      res.render("feed.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }

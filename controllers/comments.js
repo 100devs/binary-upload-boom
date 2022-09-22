3ba7688ef4c56fe92d07642b636ec6efde26de0c
Africa Mincey <africamincey@gmail.com>
2022-09-22 09:10:48 -0400

    added comment feature

--- controllers/comments.js
+++ controllers/comments.js
@@ -14,4 +14,33 @@ module.exports = {
       console.log(err);
     }
   },
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect(`/post/${req.params.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find post by id
+      let post = await Comment.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(comment.cloudinaryId);
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted comment");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
 };
+
--- routes/comments.js
+++ routes/comments.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const router = express.Router();
 const commentsController = require("../controllers/comments");
@@ -6,5 +8,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Comment Routes - simplified for now
 router.post("/createComment/:id", commentsController.createComment);
 
+router.delete("/deleteComment/:id", commentsController.deleteComment);
+
+router.put("/likeComment/:id", commentsController.likeComment);
 
 module.exports = router;

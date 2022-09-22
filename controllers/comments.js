17295186c338b1d6cb80f66407d8b1f34682baf0
artslimedev <102567983+artslimedev@users.noreply.github.com>
2022-09-22 19:50:32 -0400

    added comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,15 +1,17 @@
+
 const Comment = require("../models/Comment");
 
 module.exports = {
   createComment: async (req, res) => {
     try {
+
       await Comment.create({
         comment: req.body.comment,
         post: req.params.id,
         likes: 0,
         user: req.user.id,
       });
-      console.log("Comment has been added!");
+      console.log("Post has been added!");
       res.redirect("/post/"+req.params.id);
     } catch (err) {
       console.log(err);
@@ -17,28 +19,30 @@ module.exports = {
   },
   likeComment: async (req, res) => {
     try {
-      const comment = await Comment.findOneAndUpdate(
+      await Post.findOneAndUpdate(
         { _id: req.params.id },
         {
           $inc: { likes: 1 },
         }
       );
       console.log("Likes +1");
-      res.redirect("/post/" + comment.post);
+      res.redirect(`/post/${req.params.id}`);
     } catch (err) {
       console.log(err);
     }
   },
   deleteComment: async (req, res) => {
     try {
-      // Find Comment by id
-      let comment = await Comment.findById({ _id: req.params.id });
-      // Delete Comment from db
-      await Comment.remove({ _id: req.params.id });
-      console.log("Deleted Comment");
-      res.redirect("/post/"+ comment.post);
+      // Find post by id
+      let post = await Post.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(post.cloudinaryId);
+      // Delete post from db
+      await Post.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect("/profile");
     } catch (err) {
-      res.redirect("/post"+ comment.post);
+      res.redirect("/profile");
     }
   },
 };

502460534976a262918f727b6f4fd9cf6e3cf9ce
Cory Nelson <fullnelsontech@gmail.com>
2022-09-22 14:48:56 -0400

    comment route. first commit

---
+++ routes/comment.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+const commentsController = require("../controllers/comments")
+const { ensureAuth, ensureGuest } = require("../middleware/auth")
+
+//Comment Routes - simplified for now
+router.post("/createComment/:id", commentsController.createComment)
+
+//Delete comment route
+router.delete(
+  "/deleteComment/:postid/:commentid",
+  commentsController.deleteComment
+)
+
+module.exports = router

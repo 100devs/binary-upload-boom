2473c43a4923491097b19dc82cdc26dc9db2852b
Caleb Phillips <csphillips5@gmail.com>
2022-09-22 17:05:06 -0400

    implement ability to add comments to posts

---
+++ routes/comments.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+router.post("/addComment/:postId", commentsController.addComment);
+
+module.exports = router;

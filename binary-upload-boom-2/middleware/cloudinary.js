dd24b157fb1978b8e654ee50a2b0947c6a56ef83
Brooke Lee Kalman <98435083+BrookeLeeK@users.noreply.github.com>
2022-09-22 17:33:48 -0500

    Add files via upload

---
+++ binary-upload-boom-2/middleware/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require("cloudinary").v2;
+
+require("dotenv").config({ path: "./config/.env" });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+module.exports = cloudinary;

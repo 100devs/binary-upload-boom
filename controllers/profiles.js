4b460e7a1f7708f98520161707f3fe2e050aefb2
Nothing Remains <101616116+NothingRemains@users.noreply.github.com>
2022-09-22 16:22:04 -0600

    -Added comment functionality to posts -Added timestamp functionality (currently only implemented on comments) as middleware -Added styling to some pieces (comments, header, posts, profile)

---
+++ controllers/profiles.js
@@ -0,0 +1,16 @@
+const cloudinary = require("../middleware/cloudinary");
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const User = require("../models/User")
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+        const profile = await Post.findById(req.params.id);
+        const posts = await Post.find({ user: req.params.id });
+        res.render("profile.ejs", { posts: posts, user: req.user, paramsID: req.params.id });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};

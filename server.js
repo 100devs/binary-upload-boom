8a6fa26e04dd515f623068502f7153f2bc9357bb
Eric <gentlegiantdev@gmail.com>
2022-09-22 16:27:53 -0700

    feat: added commenting to posts capability

--- server.js
+++ server.js
@@ -10,32 +10,25 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
+const commentsRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
-
 // Passport config
 require("./config/passport")(passport);
-
 //Connect To Database
 connectDB();
-
 //Using EJS for views
 app.set("view engine", "ejs");
-
 //Static Folder
 app.use(express.static("public"));
-
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 //Logging
 app.use(logger("dev"));
-
 //Use forms for put / delete
 app.use(methodOverride("_method"));
-
 // Setup Sessions - stored in MongoDB
 app.use(
   session({
@@ -45,17 +38,15 @@ app.use(
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
-
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
 //Use flash messages for errors, info, ect...
 app.use(flash());
-
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use("/comment", commentsRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {

95113ae2511b52c44084fb86b3e35d81d436179d
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-09-23 10:29:34 +0800

    Added Comment Functionality under Post Feed View

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,12 +44,24 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout();
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
+// exports.logout = (req, res) => {
+//   req.logout();
+//   req.session.destroy((err) => {
+//     if (err)
+//       console.log("Error : Failed to destroy the session during logout.", err);
+//     req.user = null;
+
+// CodeFix by Angra974
+exports.logout = (req, res, next) => {
+  // Need a callback
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+     req.session.destroy((err) => {
+       if (err) console.log('Error : Failed to destroy the session during logout.', err);
+       req.user = null;
+     });
     res.redirect("/");
   });
 };

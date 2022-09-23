af49cddc478a0706aa4492349607e97e52887a57
najwanan <theunnamedrd@gmail.com>
2022-09-22 18:29:36 -0700

    adding controller and route for comments

---
+++ routes/comments.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/multer");
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Post Routes - simplified for now
+//router.get("/:id", ensureAuth, commentsController.getPost);
+
+router.post("/createComment", commentsController.createComment);
+
+router.put("/likeComment/:id", commentsController.likeComment);
+
+router.delete("/deleteComment/:id", commentsController.deleteComment);
+
+module.exports = router;

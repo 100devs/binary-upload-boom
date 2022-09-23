46966fe8bd89ea8e5cd89c1e0af711daacf785da
Karla Moore <Carla.Moore3@icloud.com>
2022-09-22 22:03:45 -0400

    Added route, controller, and model comments files

--- comments.js
+++ comments.js
@@ -1,31 +1,10 @@
-const Comment = require("../models/Comment");
-const User = require("../models/User");
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-module.exports = {
-  createComment: async (req, res) => {
-    try {
-      const commentUser = await User.findById(req.user.id)
-      await Comment.create({
-        comment: req.body.comment,
-        likes: 0,
-        post: req.params.id,
-        createdBy: commentUser.userName,
-        createdById: req.user.id
-      });
-      console.log("Comment has been added!");
-      res.redirect("/post/"+req.params.id);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  //! Added delete comment method
-  deleteComment: async (req, res) => {
-    try {
-      await Comment.deleteOne({ _id: req.params.commentid })
-      console.log("comment removed")
-      res.redirect("/post/"+req.params.postid);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-};
+//Comment Routes - simplified for now
+router.post("/createComment/:id", commentsController.createComment);
+
+
+module.exports = router;

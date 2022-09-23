baafc1a3987810e964be50f59595abf691f72bce
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-09-22 20:24:29 -0500

    changes

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
 const postsController = require("../controllers/posts");
+const commentsController = require("../controllers/comments");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now

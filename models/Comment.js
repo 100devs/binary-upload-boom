557caa611ecdd5de04e57f4d6f2984fd8e7cb00c
Anthony Moran <anthony771998@gmail.com>
2022-09-22 20:33:00 -0400

    Added user id to each comment model so comment creator is the only one that can delete it

--- models/Comment.js
+++ models/Comment.js
@@ -17,6 +17,10 @@ const CommentSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);

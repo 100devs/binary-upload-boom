b48c32449a214e7db21b80bbefe3c4bdd30efc4e
johanna-j-c <johanna.cabanban@gmail.com>
2022-09-22 21:01:09 -0400

    Added comments to posts view

---
+++ routes/comment.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const commentController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comment Routes - simplified for now
+// router.get("/:id", ensureAuth, postsController.getPost);
+
+router.post("/createComment/:id", commentController.createComment);
+
+// router.put("/likePost/:id", postsController.likePost);
+
+// router.delete("/deletePost/:id", postsController.deletePost);
+
+module.exports = router;

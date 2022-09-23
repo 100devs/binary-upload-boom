fd98b0ac91cf4ecef7f988e5192250f5ef9097d3
Sarah Malik <sarah.e.mower@gmail.com>
2022-09-23 12:51:09 -0700

    adding comments functionality

---
+++ models/Comment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

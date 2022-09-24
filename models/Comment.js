a6f1d7eb93c9034270f5bf658d51b5dba84300c4
Andres <andreslindo20@gmail.com>
2022-09-24 09:13:39 -0400

    added comments

---
+++ models/Comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  postId: {
+    type:String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

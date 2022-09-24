0f920f8bed82e452e07ad577f353a14d8ccc66ac
Ahmad Yosry <87082240+Gonksy@users.noreply.github.com>
2022-09-25 01:22:30 +0200

    Added functionality to show commenter username

--- controllers/posts.js
+++ controllers/posts.js
@@ -23,30 +23,32 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      const comments = await Comment.find( { postId: req.params.id } ).lean()
+      const comments = await Comment.find( { postId: req.params.id } ).populate('user', 'userName').lean()
+      console.log(comments)
       res.render("post.ejs", { post: post, user: req.user, comments: comments,  });
     } catch (err) {
       console.log(err);
     }
   },
   createPost: async (req, res) => {
-    try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
+    console.log(req.file)
+    // try {
+    //   // Upload image to cloudinary
+    //   const result = await cloudinary.uploader.upload(req.file.path);
 
-      await Post.create({
-        title: req.body.title,
-        image: result.secure_url,
-        cloudinaryId: result.public_id,
-        caption: req.body.caption,
-        likes: 0,
-        user: req.user.id,
-      });
-      console.log("Post has been added!");
-      res.redirect("/profile");
-    } catch (err) {
-      console.log(err);
-    }
+    //   await Post.create({
+    //     title: req.body.title,
+    //     image: result.secure_url,
+    //     cloudinaryId: result.public_id,
+    //     caption: req.body.caption,
+    //     likes: 0,
+    //     user: req.user.id,
+    //   });
+    //   console.log("Post has been added!");
+    //   res.redirect("/profile");
+    // } catch (err) {
+    //   console.log(err);
+    // }
   },
   likePost: async (req, res) => {
     try {

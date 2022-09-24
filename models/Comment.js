72569fe36cc332b0e6ff97dcf75caae711dee283
alcarreon <alcarreon20@gmail.com>
2022-09-23 23:19:37 -0400

    updated  to include a comment model

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  commment: {
+    type: String,
+    required: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

ea89ba37c7f660aecaf43d0c94da342c65c45c92
Monica Vinluan <mdcam.prod@gmail.com>
2022-09-25 22:58:37 -0700

    minor improvements

--- controllers/comments.js
+++ controllers/comments.js
@@ -7,7 +7,6 @@ module.exports = {
         comment: req.body.comment,
         post: req.params.id,
         likes: 0,
-        user: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);

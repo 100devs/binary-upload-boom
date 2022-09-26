963bb7f104dc64fb243b84a84b323e1594c6149f
Shire224 <shire_hassen224@hotmail.com>
2022-09-26 11:47:25 -0400

    added Usernames, Likes, and delete

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,12 +1,18 @@
-const Comment = require("../models/Comments");
 
+const Post = require("../models/Post");
+const Comment = require("../models/Comments");
+//const cloudinary = require("../middleware/cloudinary");
 module.exports = {
   createComment: async (req, res) => {
     try {
+      //TODO - add new properties to each document
+      //const commentUSer = await User.findById(req.user.id);
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
+        createdBy: req.user.userName,
+        createdById: req.user.id
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);
@@ -14,4 +20,26 @@ module.exports = {
       console.log(err);
     }
   },
-};
+  deleteComments: async (req, res) => {
+    try {
+      await Comment.deleteOne({_id: req.params.commentid})
+      res.redirect("/post/"+req.params.id);
+    } catch(err) {
+      console.log(err)
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.commentid },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect("/post/"+req.params.postid);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+}
--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Comment Routes - simplified for now
 
 router.post("/createComment/:id", commentsController.createComment);
+router.put("/likeComment/:postid/:commentid", commentsController.likeComment);
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComments)
 
 module.exports = router;

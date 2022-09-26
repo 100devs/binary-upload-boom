81a1e9dab57f07be21c39a7839a652f87ffba0e3
Shire224 <shire_hassen224@hotmail.com>
2022-09-25 21:14:04 -0400

    added Comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comments");
+
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -7,7 +9,7 @@ module.exports = {
       const posts = await Post.find({ user: req.user.id });
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
-      console.log(err);
+      console.log(err); 
     }
   },
   getFeed: async (req, res) => {
@@ -21,7 +23,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }

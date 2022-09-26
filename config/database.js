4373d75e387520d7b7956ce325e2a3294b828528
Ryan <ryanmahara@gmail.com>
2022-09-26 17:08:37 -0500

    add comments to database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // DB connection server call
     const conn = await mongoose.connect(process.env.DB_STRING, {
+      // old settings required for old mongoDB
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
@@ -12,6 +14,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
+    // kill node process
     process.exit(1);
   }
 };

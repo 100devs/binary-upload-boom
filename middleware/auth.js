119755858459e43ce92ea52329ea34090c1f461d
Ryan <ryanmahara@gmail.com>
2022-09-27 12:12:40 -0500

    add commments to auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 module.exports = {
   ensureAuth: function (req, res, next) {
     if (req.isAuthenticated()) {
+      // next middleware
       return next();
     } else {
-      res.redirect("/");
+      res.redirect('/');
     }
   },
   ensureGuest: function (req, res, next) {
     if (!req.isAuthenticated()) {
+      // next middleware
       return next();
     } else {
-      res.redirect("/dashboard");
+      res.redirect('/dashboard');
     }
   },
 };

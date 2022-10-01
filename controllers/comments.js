0239a49d890f38b1e141d1c1c862f789328b2860
t0md3v <tom.infotek@gmail.com>
2022-10-01 14:35:33 -0600

    add comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,17 +1,15 @@
-const cloudinary = require("../middleware/cloudinary");
-const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 
 module.exports = {
   createComment: async (req, res) => {
     try {
-      await Post.create({
+      await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
       });
       console.log("Post has been added!");
-      res.redirect("/profile");
+      res.redirect("/post/"+req.params.id);
     } catch (err) {
       console.log(err);
     }

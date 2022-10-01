79859261b7eb3b64382d4291fb39787b5c4550e7
t0md3v <tom.infotek@gmail.com>
2022-10-01 14:49:21 -0600

    add comment controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,7 +8,7 @@ module.exports = {
         likes: 0,
         post: req.params.id,
       });
-      console.log("Post has been added!");
+      console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);
     } catch (err) {
       console.log(err);

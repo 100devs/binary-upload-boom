04fbbd922966afb81c60d5e8e9efa8351491a604
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-02 18:38:49 -0500

    added comments to each line

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require("express"); //helps us build out our API
 const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const methodOverride = require("method-override");
-const flash = require("express-flash");
-const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
-//const commentRoutes = require("./routes/comments");
+const mongoose = require("mongoose"); //helps us talk to our MONGO DB
+const passport = require("passport"); //uses for authentication. Strats for logins
+const session = require("express-session"); //allows users to stay logged in across app
+const MongoStore = require("connect-mongo")(session); //stores our actual session in MONGO DB
+const methodOverride = require("method-override"); //allows us to use GET & POST throughout our entire app but treat them as PUT & DELETE as needed to do different things
+const flash = require("express-flash"); //shows all our notifications
+const logger = require("morgan"); //logs everything happening in console
+const connectDB = require("./config/database"); //connects to our DB
+const mainRoutes = require("./routes/main"); //routes for homepage,login,signup
+const postRoutes = require("./routes/posts"); //routes for individual posts,feed,profile
+//const commentRoutes = require("./routes/comments"); //routes for comments
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });

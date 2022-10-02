7f0eb83efa0a04d07f4046576adb2a9472a92149
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-02 18:52:22 -0500

    added more comments

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express"); //helps us build out our API
+const express = require("express"); //helps build out API
 const app = express();
 const mongoose = require("mongoose"); //helps us talk to our MONGO DB
 const passport = require("passport"); //uses for authentication. Strats for logins
@@ -6,7 +6,7 @@ const session = require("express-session"); //allows users to stay logged in acr
 const MongoStore = require("connect-mongo")(session); //stores our actual session in MONGO DB
 const methodOverride = require("method-override"); //allows us to use GET & POST throughout our entire app but treat them as PUT & DELETE as needed to do different things
 const flash = require("express-flash"); //shows all our notifications
-const logger = require("morgan"); //logs everything happening in console
+const logger = require("morgan"); //logs & shows everything happening in console
 const connectDB = require("./config/database"); //connects to our DB
 const mainRoutes = require("./routes/main"); //routes for homepage,login,signup
 const postRoutes = require("./routes/posts"); //routes for individual posts,feed,profile
@@ -22,22 +22,22 @@ require("./config/passport")(passport);
 connectDB();
 
 //Using EJS for views
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //server now expects EJS files to be whats used to spit out our VIEWS
 
 //Static Folder
-app.use(express.static("public"));
+app.use(express.static("public")); //for any static assets(CSS,JS,IMGS) files
 
-//Body Parsing
+//Body Parsing - allows us to pull stuff out of requests being made (like when submitting a form and pulling the data out of those forms)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Logging
+//Logging - using MORGAN to log requests in console
 app.use(logger("dev"));
 
 //Use forms for put / delete
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method")); //allows to OVERRIDE any request that comes to server
 
-// Setup Sessions - stored in MongoDB
+//Setup Sessions - stored in MongoDB - needed for session to be implimented
 app.use(
   session({
     secret: "keyboard cat",
@@ -47,7 +47,7 @@ app.use(
   })
 );
 
-// Passport middleware
+// Passport middleware - helps with authentication, allows user to stay logged in
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -61,5 +61,5 @@ app.use("/post", postRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+  console.log(`Server is runninghttp://localhost:${process.env.PORT}/`);
 });

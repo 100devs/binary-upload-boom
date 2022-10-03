3153245fa334164123e6f4b53859acb3e52fd71f
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-10-02 19:14:52 -0500

    added comments for assistance

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,12 @@
-const express = require("express");
+const express = require("express"); //initiates routes
 const router = express.Router();
-const upload = require("../middleware/multer");
-const postsController = require("../controllers/posts");
-//add comments controller
-const commentsController = require("../controllers/comments")
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const upload = require("../middleware/multer"); //helps upload images
+const postsController = require("../controllers/posts"); // where to find posts controller
+const commentsController = require("../controllers/comments"); //adds comments controller
+const { ensureAuth, ensureGuest } = require("../middleware/auth"); //checks whether someone is logged in or not
 
 //Post Routes - simplified for now
-router.get("/:id", ensureAuth, postsController.getPost);
+router.get("/:id", ensureAuth, postsController.getPost); // the colon with name after it gives you a variable you can use inside of your controller that will grab that value
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
 

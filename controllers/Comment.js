c0ff4fa1a06d02d5643ec1cf7ccfaed92d0529ec
Erich Sabo <erich.sabo@gmail.com>
2022-10-04 02:06:48 +0300

    updated controller

--- controllers/Comment.js
+++ controllers/Comment.js
@@ -1,27 +1,46 @@
-const mongoose = require("mongoose");
+const Comment = require("../models/Comments");
 
-const CommentSchema = new mongoose.Schema({
-  comment: {
-    type: String,
-    required: true,
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        user: req.user.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
   },
-  //add in ability to tie username to the comment
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
-  },
-  likes: {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
+//   likePost: async (req, res) => {
+//     try {
+//       await Post.findOneAndUpdate(
+//         { _id: req.params.id },
+//         {
+//           $inc: { likes: 1 },
+//         }
+//       );
+//       console.log("Likes +1");
+//       res.redirect(`/post/${req.params.id}`);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   deletePost: async (req, res) => {
+//     try {
+//       // Find post by id
+//       let post = await Post.findById({ _id: req.params.id });
+//       // Delete image from cloudinary
+//       await cloudinary.uploader.destroy(post.cloudinaryId);
+//       // Delete post from db
+//       await Post.remove({ _id: req.params.id });
+//       console.log("Deleted Post");
+//       res.redirect("/profile");
+//     } catch (err) {
+//       res.redirect("/profile");
+//     }
+//   },
+};

bdc3369b7732ff1ec1190456b3b2b3504a6e5769
Tony Ly <tony.m.ly7@gmail.com>
2022-10-02 23:16:49 -0400

    tried customizing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,14 @@ module.exports = {
     plugins: [
       require[("daisyui")],
     ]
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+      prefix: "",
+      darkTheme: "dark",
+    },
   }

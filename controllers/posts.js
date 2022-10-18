bddde4e957a2a4a157ed77eb7940430ef6207215
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-10-18 02:41:16 -0400

    updated deprecated code

--- controllers/posts.js
+++ controllers/posts.js
@@ -108,7 +108,7 @@ module.exports = {
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
-      await Post.remove({ _id: req.params.id });
+      await Post.deleteOne({ _id: req.params.id });
       console.log("Deleted Post");
       res.redirect("/profile");
     } catch (err) {

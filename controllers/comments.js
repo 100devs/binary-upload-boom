a1dbcdd959dc275b9e6b29268c337f2d3b146460
saira512dev <512saira@gmail.com>
2022-11-01 21:48:09 +0530

    added functionality to add and delete comments

---
+++ controllers/comments.js
@@ -0,0 +1,28 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+      console.log("HERE")
+    try {
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};

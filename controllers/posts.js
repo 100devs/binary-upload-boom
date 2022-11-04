16a2a5875ee57e794a4b2ee572ab72541e205375
pascalecodes <pdelauna@gmail.com>
2022-11-03 20:44:57 -0700

    edits to feed and post various for render

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,14 +14,15 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      res.render("feed.ejs", { posts: posts, user: req.user});
     } catch (err) {
       console.log(err);
     }
   },
   getPost: async (req, res) => {
     try {
-      const post = await Post.findById(req.params.id);
+      const post = await Post.findById(req.params.id).populate('user');
+      
       // const comments = await Comment.find(post+req.params.id).sort({ createdAt: "desc" }).lean();
       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
       res.render("post.ejs", { post: post, user: req.user, comments: comments});
@@ -33,7 +34,7 @@ module.exports = {
     try {
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
-
+    
       await Post.create({
         title: req.body.title,
         image: result.secure_url,

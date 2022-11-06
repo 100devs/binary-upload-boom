5c4e030138ac49278e3c45d36ba7b028e4b26c4f
AyobamiH <heryourbarme@live.com>
2022-11-06 14:35:41 +0000

    Added passpot strategy for google OAUTH20

--- config/passport.js
+++ config/passport.js
@@ -39,3 +39,46 @@ module.exports = function (passport) {
     User.findById(id, (err, user) => done(err, user));
   });
 };
+
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        }
+
+        try {
+          let user = await GoogleUser.findOne({ googleId: profile.id })
+
+          if (user) {
+            done(null, user)
+          } else {
+            user = await GoogleUser.create(newUser)
+            done(null, user)
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    )
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    GoogleUser.findById(id, (err, user) => done(err, user))
+  })
+}

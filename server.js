1c4d35dc001b8b818e4dfec0747ecc4b2eeec371
Boobeh123 <boobeh321@gmail.com>
2022-11-10 11:04:22 -1000

    Heroku deployment

--- server.js
+++ server.js
@@ -10,20 +10,13 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
-
-//Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
-
-// Passport config
 require("./config/passport")(passport);
 
-//Connect To Database
 connectDB();
 
-//Using EJS for views
-app.set("view engine", "ejs");
 
-//Static Folder
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Body Parsing
@@ -53,11 +46,10 @@ app.use(passport.session());
 //Use flash messages for errors, info, ect...
 app.use(flash());
 
-//Setup Routes For Which The Server Is Listening
+// Routes
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 
-//Server Running
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
 });

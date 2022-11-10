9cc849fd48b005062d92753042c1f64e46b6e1e4
AyobamiH <heryourbarme@live.com>
2022-11-10 13:09:36 +0000

    new files and work

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,7 @@ const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require('../models/Comment')
 
+
 module.exports = {
   getProfile: async (req, res) => {
     try {
@@ -14,7 +15,8 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({createdAt: "desc"}).lean();
-      res.render("feed.ejs", { posts: posts });
+   
+      res.render("feed.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }

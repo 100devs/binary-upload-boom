c908a34d909b1e80d4d60a646b3f57d4186b288b
Cristobal Evensen <cevensen.a@gmail.com>
2023-01-23 20:42:05 -0300

    Added comment functionality on posts

--- routes/posts.js
+++ routes/posts.js
@@ -13,4 +13,6 @@ router.put("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePost/:id", postsController.deletePost);
 
+router.post("/addComment/:id", postsController.addComment)
+
 module.exports = router;

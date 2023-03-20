8cf018dca2c43f6fe3c7c6c2f9190a46feec517f
Ashish Tom <ashmint@gmail.com>
2023-03-20 22:21:05 +0530

    changes for cyclic

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/passport")(passport);
 
 //Connect To Database
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+      console.log("Server is running, you better catch it!");
+  })
+})
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -60,6 +64,6 @@ app.use("/post", postRoutes);
 app.use("/comments", commentsRoutes);
 
 //Server Running
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
-});
+// app.listen(process.env.PORT, () => {
+//   console.log("Server is running, you better catch it!");
+// });

aff54dc93b96a3c7a8266f1d1e217df502205ac8
Gian Carnero <gian.carnero@gmail.com>
2023-03-28 15:20:56 -0500

    added comment functionality

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,8 @@ router.get("/:id", ensureAuth, postsController.getPost);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
+router.post("/commentPost/:id", postsController.commentPost);
+
 router.put("/likePost/:id", postsController.likePost);
 
 router.delete("/deletePost/:id", postsController.deletePost);

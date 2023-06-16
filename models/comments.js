c19137f01cd80fc104d4a15baa35eb082ad78f55
robinetta <alxr19@gmail.com>
2023-06-15 23:24:26 -0400

    added comments feature

---
+++ models/comments.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const express = require ("express")
+const router = express.Router()
+const commentController = require("../controllers/comments")
+
+
+router.post('/createComment/:id', commentController.createComment)
+

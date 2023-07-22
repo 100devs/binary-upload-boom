2bff6106342456cda86872f6f1714a2d7d58da3b
Phil Kearney <devphiliper@gmail.com>
2023-07-22 16:32:47 +0100

    removed commented out code

--- controllers/comments.js
+++ controllers/comments.js
@@ -29,18 +29,4 @@ module.exports = {
       console.log(err);
     }
   },
-//   deletePost: async (req, res) => {
-//     try {
-//       // Find post by id
-//       let post = await Post.findById({ _id: req.params.id });
-//       // Delete image from cloudinary
-//       await cloudinary.uploader.destroy(post.cloudinaryId);
-//       // Delete post from db
-//       await Post.remove({ _id: req.params.id });
-//       console.log("Deleted Post");
-//       res.redirect("/profile");
-//     } catch (err) {
-//       res.redirect("/profile");
-//     }
-//   },
 };

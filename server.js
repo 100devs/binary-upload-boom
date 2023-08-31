31b2ed53f5ab51e4602e653a63ae07d971eea5b3
Garrett Stewart <garrettbstewart@gmail.com>
2023-08-31 14:04:33 -0400

    added comments section

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const logger = require("morgan"); // Requiring Morgan
 const connectDB = require("./config/database"); // Requiring Database Config File
 const mainRoutes = require("./routes/main"); // Main Routes
 const postRoutes = require("./routes/posts"); // Post Routes
+const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -57,6 +58,7 @@ app.use(flash());
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {

c09737cce06636f87c635f6daa73b02d69317c13
Galitie <galitdances@gmail.com>
2023-08-31 18:35:07 -0400

    Added ability to add comments to posts

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,16 +1,15 @@
-const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 
 module.exports = {
   createComment: async (req, res) => {
     try {
-      await Post.create({
+      await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
       });
       console.log("Comment has been added!");
-      res.redirect("/post");
+      res.redirect("/post/" + req.params.id);
     } catch (err) {
       console.log(err);
     }

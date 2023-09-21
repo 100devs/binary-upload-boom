f1c7e8a6a2a75eaff40b330720b545bd32d7a4a1
huffvii <huffvii@gmail.com>
2023-09-21 18:28:58 -0500

    adding comments

---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const commentsController = require("../controllers/comments");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Comment Routes - simplified for now
+
+router.post("/createComment", commentsController.createComment);
+
+
+module.exports = router;

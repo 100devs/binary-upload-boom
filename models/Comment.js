e6c712cbf5dfd4a97de747cbea29d55346ffa8e8
vmalaviya <varunmalaviya7@gmail.com>
2023-10-09 17:57:09 -0400

    added ability to add comments. Dusty1901 on discord

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);

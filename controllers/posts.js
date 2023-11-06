aebe80b062d35ef66460d74f5e8631815ef8fe88
KKDEVE <116938392+Codedkk@users.noreply.github.com>
2023-11-06 21:25:56 +0800

    comment

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,15 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      const post = await Post.findById(req.params.id);
+      res.render("post.ejs", { post: post, user: req.user });
+      const comments = await Comment.find({ post: req.params.id }).sort({ createdAt: "desc" }).lean()
+      res.render("post.ejs", { post: post, user: req.user, comments: comments });
     } catch (err) {
       console.log(err);
     }

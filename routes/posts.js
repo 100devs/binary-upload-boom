24e8867a9f832380fd8b22ea23220a33f91f57a9
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-12-27 14:57:23 +0200

    nothing changed, just minor saves while reviewing a code

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const upload = require("../middleware/multer");
-const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const upload = require('../middleware/multer');
+const postsController = require('../controllers/posts');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 //Post Routes - simplified for now
-router.get("/:id", ensureAuth, postsController.getPost);
+router.get('/:id', ensureAuth, postsController.getPost);
 
-router.post("/createPost", upload.single("file"), postsController.createPost);
+router.post('/createPost', upload.single('file'), postsController.createPost);
 
-router.put("/likePost/:id", postsController.likePost);
+router.put('/likePost/:id', postsController.likePost);
 
-router.delete("/deletePost/:id", postsController.deletePost);
+router.delete('/deletePost/:id', postsController.deletePost);
 
 module.exports = router;

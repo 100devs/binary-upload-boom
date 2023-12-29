e322dba06aab3b99af130018c1cee921d4c280ef
Kwame Cody <kwamecody@gmail.com>
2023-12-29 09:14:45 +0000

    added user names to each post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
+const User = require("../models/User")
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -13,7 +14,7 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      res.render("feed.ejs", { posts: posts});
     } catch (err) {
       console.log(err);
     }
@@ -21,7 +22,7 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
+      res.render("post.ejs", { post: post, user: req.user, userName:post.userName});
     } catch (err) {
       console.log(err);
     }
@@ -38,6 +39,7 @@ module.exports = {
         caption: req.body.caption,
         likes: 0,
         user: req.user.id,
+        userName : req.user.userName
       });
       console.log("Post has been added!");
       res.redirect("/profile");
